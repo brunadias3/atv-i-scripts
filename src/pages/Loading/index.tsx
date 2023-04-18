c1de7674af77e98472dd869dd6759ccda527fa98
Bruna <brunadias_30@hotmail.com>
2023-04-18 09:41:29 -0300

    alterando loading page

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -4,7 +4,7 @@ import './index.css'
 export default function Loading() {
     return (
         <div className='body-loader'>
-            <div className="loader"></div>
+            <span>Carregando...</span>
         </div>
     )
 }
