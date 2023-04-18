4425e97b296ceb48cfbfc4e9e3396563a13af09a
Bruna <brunadias_30@hotmail.com>
2023-04-18 11:35:20 -0300

    estilizando menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,26 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import './index.css'
 
 export default function Menu() {
-  // const location = useLocation().pathname;
-  // console.log(location)
+  const location = useLocation().pathname;
+
   return (
     <div className='menu'>
-      <span><Link to="/lotofacil">Lotofácil</Link></span>
-      <span><Link to="/megasena">Megasena</Link></span>
-      <span><Link to="/quina">Quina</Link></span>
+      <span>
+        <Link to="/lotofacil" className={location === "/lotofacil" ? "isActive" : "corLoto"}>
+          Lotofácil
+        </Link>
+      </span>
+      <span>
+        <Link to="/megasena" className={location === "/megasena" ? "isActive" : "corMega"}>
+          Megasena
+        </Link>
+      </span>
+      <span>
+        <Link to="/quina" className={location === "/quina" ? "isActive" : "corQuina"}>
+          Quina
+        </Link>
+      </span>
     </div>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,14 @@ import Lotofacil from "../pages/lotofacil/Lotofacil";
 import Quina from "../pages/quina/Quina";
 import Menu from "../components/Menu/Menu";
 import Erro from "../pages/Erro";
+import Loading from "../pages/Loading";
 
 export default function Rotas({currentPage}:any) {
   return (
     <BrowserRouter>
         <Menu />
           <Routes>
+            <Route path="/" element={<div></div>} />
             <Route path="/megasena" element={<Megasena />} />
             <Route path="/lotofacil" element={<Lotofacil />} />
             <Route path="/quina" element={<Quina />} />
